9664c94b934373ba62ef8394523955f371ce3592
Daniel Ferrada <daniel.ferradabarria@gmail.com>
2024-03-13 22:46:08 -0300

    html and js done falta css

--- src/views/Contacto.jsx
+++ src/views/Contacto.jsx
@@ -1,7 +1,26 @@
-const Contact = () => {
-    return(
-        <div>contacto</div>
-    )
+import Button from 'react-bootstrap/Button'
+import Form from 'react-bootstrap/Form'
+
+function Contact() {
+  return (
+    <Form>
+        <h1>Cuentanos, en que te podemos ayudar?</h1>
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Correo</Form.Label>
+        <Form.Control type="email" placeholder="name@example.com" />
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="formBasicPassword">
+        <Form.Label>Descripcion</Form.Label>
+        <Form.Control type="text" placeholder="" />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formBasicCheckbox">
+      </Form.Group>
+      <Button variant="danger" type="submit">
+        Enviar
+      </Button>
+    </Form>
+  );
 }
 
 export default Contact
--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,6 +1,10 @@
 const Home = () => {
     return (
-        <div>Home</div>
+        <div>
+            <h1>Bienvenido a <span>Happy Cake</span></h1>
+            <p>El lugar de los pasteles felices</p>
+            <img src="https://img.icons8.com/cotton/64/birthday.png" alt="Pastel"/>
+        </div>
     )
 }
 
